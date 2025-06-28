42c5dded72251110026349b5db9ef70c198520ea
AgustinaGuitmanD <agustinaguitmandallariva@gmail.com>
2025-06-28 18:54:00 -0300

    Update ArchivoRepository.java

--- src/main/java/org/example/models/repositories/impl/ArchivoRepository.java
+++ src/main/java/org/example/models/repositories/impl/ArchivoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public class ArchivoRepository {
+public class ArchivoRepository implements IArchivoRepository{
     private List<Archivo> archivos;
     public void save(String name, Directorio ubicacion) {
         // TODO
